ae928b401fd42816799ea7a57106a035689f10ad
IkkiRama <rifkivamaus@gmail.com>
2023-05-28 00:00:12 +0700

    Refactor

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+let scrollTop = document.querySelector("#scroll-top");
+
 window.addEventListener("scroll", () => {
-    let scrollTop = document.querySelector("#scroll-top")
-    let posisi = window.scrollY > 200;
-    scrollTop.classList.toggle("show", posisi)
-})
+  let posisi = window.scrollY > 200;
+  scrollTop.classList.toggle("show", posisi);
+  if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    scrollTop.classList.toggle("show");
+  }
+});
+
+const toTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
+scrollTop.addEventListener("click", () => toTop());
